58c9410c1969fe027121a80bffbde9bc59016515
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2024-05-22 23:34:00 +0200

    V1.0
    mostly complete, updating spots is missing tough and could use some better form and color.

--- app/src/main/java/com/example/wavefinder/MainActivity.kt
+++ app/src/main/java/com/example/wavefinder/MainActivity.kt
@@ -2,7 +2,6 @@
 
 package com.example.wavefinder
 
-import AddSpot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- app/src/main/java/com/example/wavefinder/apiservice/ApiClient.kt
+++ app/src/main/java/com/example/wavefinder/apiservice/ApiClient.kt
@@ -8,7 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://192.168.75.122:8080"
+    private const val BASE_URL = "http://192.168.1.83:8080"
+
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
